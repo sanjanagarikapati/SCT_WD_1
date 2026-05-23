42bcba2dda20b1198a04704d7e642ed53137d490
sanjanagarikapati <sanjanagarikapati9999@gmail.com>
2026-05-23 15:28:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Change navbar style on scroll
+
+window.addEventListener("scroll", function(){
+
+  const navbar = document.getElementById("navbar");
+
+  if(window.scrollY > 50){
+    navbar.classList.add("scrolled");
+  }
+  else{
+    navbar.classList.remove("scrolled");
+  }
+
+});
